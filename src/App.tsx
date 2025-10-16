723ce2e76054f71fa8c211e8b73ec77c9068a907
Brian DeLancey <brian.delancey@archerirm.com>
2025-10-16 15:37:46 -0500

    fix bad comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from 'react';
+import type { Schema } from '../amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -14,20 +14,22 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: window.prompt('Todo content') });
   }
-  
+
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+    client.models.Todo.delete({ id });
   }
-  
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -40,7 +42,5 @@ function App() {
     </main>
   );
 }
-
-'// Test comment
 
 export default App;
